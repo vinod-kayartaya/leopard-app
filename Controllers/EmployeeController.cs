6a199470b33b601d2f972af723b0001a94f860a7
Vinod Kumar Kayartaya <vinod@vinod.co>
2024-12-20 11:50:32 +0530

    first commit

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace admin_web.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+} 
